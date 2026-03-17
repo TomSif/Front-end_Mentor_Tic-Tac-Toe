a797b383fb0acd7ffc7960cb2dbdf08009b9e8b1
TomSif <thomas.sifferle.programmer@gmail.com>
2026-03-17 08:30:57 +0100

    chore(set-up): first commit, react, typescrip and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <h1 className="text-cyan-600">TEST Tailwind</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
